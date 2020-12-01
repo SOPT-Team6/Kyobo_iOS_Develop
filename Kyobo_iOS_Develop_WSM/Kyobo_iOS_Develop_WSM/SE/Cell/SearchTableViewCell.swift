d3aee68b5f25501fbcca626abd031592ef304b30
seri <mas1jacoke@naaver.com>
2020-12-02 02:25:35 +0900

    아직덜했음

---
+++ Kyobo_iOS_Develop_WSM/Kyobo_iOS_Develop_WSM/SE/Cell/SearchTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  SearchTableViewCell.swift
+//  Kyobo_iOS_Develop_WSM
+//
+//  Created by Seri Park on 2020/12/01.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    static let identifier = "SearchTableViewCell"
+
+    @IBOutlet var bookimageView: UIImageView!
+    @IBOutlet var ebookLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var writerLabel: UILabel!
+    @IBOutlet var explainLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setCell (book : Book) {
+        bookimageView.image = book.makeBookImage()
+        ebookLabel.text = book.ebook
+        nameLabel.text = book.name
+        writerLabel.text = book.writer
+        explainLabel.text = book.explain
+    }
+
+}
--- Kyobo_iOS_Develop_WSM/Kyobo_iOS_Develop_WSM/SE/ViewController/SearchVC.swift
+++ Kyobo_iOS_Develop_WSM/Kyobo_iOS_Develop_WSM/SE/ViewController/SearchVC.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    @IBOutlet var serahTableView: UITableView!
+    @IBOutlet var searchTextField: UITextField!
+    
+    var bookSearch: [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBookData()
+        searchTextField.layer.cornerRadius = 8
+        searchTextField.backgroundColor = .veryLightPink
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setBookData() {
+        bookSearch.append(contentsOf: [
+            Book
+        ])
     }
-    */
+  
 
 }
